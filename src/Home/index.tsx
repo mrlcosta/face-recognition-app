548cf3285a0fb30930f4599587f63df3302c3cd5
WalberCesar <walber.santos01@etec.sp.gov.br.com>
2025-01-20 06:35:39 -0300

    first commit

---
+++ src/Home/index.tsx
@@ -0,0 +1,99 @@
+import { 
+  Text, 
+  useWindowDimensions, 
+  View 
+} from 'react-native'
+import { 
+  useEffect, 
+  useState,
+  useRef
+} from 'react'
+import {
+  Frame,
+  useCameraDevice,
+  useCameraPermission,
+  Camera as VisionCamera
+} from 'react-native-vision-camera'
+
+import { styles } from './styles'
+import { Face, FaceDetectionOptions, Camera } from 'react-native-vision-camera-face-detector'
+
+export  function Home() {
+
+  const [faceDetected, setFaceDetected] = useState<boolean>(false)
+
+  const {
+    hasPermission,
+    requestPermission
+  } = useCameraPermission()
+
+  if(!hasPermission) {
+    console.log('Not camera permission')
+  }
+
+  const {
+    width,
+    height
+  } = useWindowDimensions()
+  
+
+  
+
+  const faceDetectionOptions = useRef<FaceDetectionOptions>( {
+    performanceMode: 'fast', 
+    classificationMode:'all'
+
+  } ).current
+
+
+  const device = useCameraDevice('front')
+
+  if(!device){
+    console.log('device =>', device)
+    return
+  }
+
+
+  function handleFacesDetection(
+    faces: Face[],
+    frame: Frame
+  ): void { 
+    
+    const face = faces[0] 
+
+    if (face) {
+
+      // console.log(face)
+      setFaceDetected(true) 
+
+      
+      if(face.leftEyeOpenProbability > 0.5 && face.rightEyeOpenProbability < 0.5){
+        console.log('piscou o olho ESQUERDO')
+       
+      }
+      if(face.leftEyeOpenProbability < 0.5 && face.rightEyeOpenProbability > 0.5){
+        console.log('piscou o olho DIREITO')
+      }
+
+    }else {
+      setFaceDetected(false)
+    }
+
+    
+
+   
+  }
+
+  return (
+    <View style={styles.container}>
+       <Camera
+        isActive={true}
+        style={styles.camera}
+        device={device}   
+        faceDetectionCallback={handleFacesDetection}
+        faceDetectionOptions={faceDetectionOptions}
+        
+     />
+    </View>
+  )
+}
